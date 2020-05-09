4dbb9fd3ccb071098ed13f823c20e6e17a41f071
Gautham-N <58910261+Gautham-N@users.noreply.github.com>
2020-05-09 13:04:51 +0530

    Update blobdetection.py

--- blobdetection.py
+++ blobdetection.py
@@ -19,5 +19,6 @@ detector = cv2.SimpleBlobDetector_create()
 keypoints = detector.detect(img)
 im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 Processed_img=im_with_keypoints;
+#Displaying Processed image
 cv2_imshow(Processed_img)
 cv2.waitKey(0)
